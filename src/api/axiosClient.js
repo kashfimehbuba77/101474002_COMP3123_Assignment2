27707ef2856e71753770a16836dac331e5fd1431
Kashfi Mehbuba <kashfimehbuba71@gmail.com>
2025-11-28 18:28:33 -0500

    Fix API base URL

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,9 +2,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://101474002-comp-3123-assignment02.vercel.app/api/v1",
+  baseURL: "https://101474002-comp-3123-assignment01.vercel.app/api/v1",
 });
 
+// Attach token if you add JWT later
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
